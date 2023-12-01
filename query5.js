ca2f6071b02b59bc392c5dfe38c5983a94e4075d
Charlotte <charlottemay.234@gmail.com>
2023-11-30 20:15:07 -0800

    commented out query5

--- query5.js
+++ query5.js
@@ -1,11 +1,11 @@
-5200_proj2.Product.aggregate([
-    {
-      $match:
-        /**
-         * query: The query in MQL.
-         */
-        {
-          type: "serum",
-        },
-    },
-  ]);
+// 5200_proj2.Product.aggregate([
+//     {
+//       $match:
+//         /**
+//          * query: The query in MQL.
+//          */
+//         {
+//           type: "serum",
+//         },
+//     },
+//   ]);
