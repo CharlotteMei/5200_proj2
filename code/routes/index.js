2951a3c22349af9d2f7b55b996c285280a106c35
Charlotte <charlottemay.234@gmail.com>
2023-12-03 20:54:33 -0800

    add product with no product_id field as it would be replaced by mongodb id in the future implementation

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { readAllProducts } = require('../db/mongo_queries.js');
+const { readAllProducts, createProduct } = require('../db/mongo_queries.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,13 +10,24 @@ router.get('/', function(req, res, next) {
 /* product page. */
 router.get('/products', async function(req, res, next) {
   const products = await readAllProducts();
-  console.log(products);
+  // console.log(products);
   res.render('products', {products: products});
 });
 
 /* product add page. */
+router.get('/products/add', function(req, res, next) {
+  res.render('product_add');
+});
+
+router.post('/products/add', async function(req, res, next) {
+  const result = await createProduct(req.body);
+  console.log(result)
+  res.redirect('/products');
+});
 
 /* product edit page. */
+
+/* product delete. */
 
 
 /* rack page. */
